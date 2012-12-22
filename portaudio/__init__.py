6107742c1675c3a2e66cdb99e4773ba88965c6fc
Elijah Rutschman <elijahr@gmail.com>
2012-12-21 21:34:05 -0600

    clean up initialization

--- portaudio/__init__.py
+++ portaudio/__init__.py
@@ -7,8 +7,9 @@ class PortAudio(object):
     Context manager for using PortAudio
     """
     def __enter__(self):
+        err = _portaudio.Pa_Initialize()
         try:
-            raise get_exception(_portaudio.Pa_Initialize())
+            raise get_exception(err)
         except NoError:
             pass
         return self
